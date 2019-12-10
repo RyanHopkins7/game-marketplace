33fefa61f04ec0147f40f870347839da12243caf
RyanHopkins7 <ryan.hopkins@students.makeschool.com>
2019-12-09 22:49:44 -0800

    Added all routes

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('submit-game', views.submit_game, name='submit game'),
+    path('search', views.search, name='search'),
+    path('game', views.game_detail, name='individual game detail'),
 ]
--- marketplace/views.py
+++ marketplace/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def submit_game(request):
+    return render(request, 'sell.html')
+
+def search(request):
+    return render(request, 'search.html')
+
+def game_detail(request):
+    return render(request, 'individual_game.html')
