ca27d9d110a543cae4901f615651245e5f2ceefb
RyanHopkins7 <ryan.hopkins@students.makeschool.com>
2019-12-11 07:46:18 -0800

    Fix login

--- accounts/views.py
+++ accounts/views.py
@@ -6,5 +6,5 @@ from django.urls import reverse_lazy
 
 class Signup(CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('/')
+    success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
