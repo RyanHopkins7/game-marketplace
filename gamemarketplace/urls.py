176a428f9f77ad499da5a3a1030ed578630196e2
RyanHopkins7 <ryan.hopkins@students.makeschool.com>
2019-12-11 08:01:24 -0800

    Add static file routes

--- gamemarketplace/urls.py
+++ gamemarketplace/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     path('', include('marketplace.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
